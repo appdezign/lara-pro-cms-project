7d16d7ce66e6c518dba4a27cecc94f51f38d29d7
Sybrand Hoeksma <s.hoeksma@firmaq.nl>
2026-01-27 18:18:58 +0100

    Add cached routes

--- laracms/app/Providers/LaraAppServiceProvider.php
+++ laracms/app/Providers/LaraAppServiceProvider.php
@@ -2,18 +2,26 @@
 
 namespace Lara\App\Providers;
 
+use Illuminate\Foundation\Support\Providers\RouteServiceProvider;
 use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
+
 
 use Lara\Common\Models\Entity;
 
 use Lara\Common\Http\Controllers\Setup\Concerns\HasSetup;
 
+use Mcamara\LaravelLocalization\Traits\LoadsTranslatedCachedRoutes;
+
 class LaraAppServiceProvider extends ServiceProvider
 {
 
 	use HasSetup;
+	use LoadsTranslatedCachedRoutes;
+
+
 	/**
 	 * Bootstrap the module services.
 	 *
@@ -21,6 +29,9 @@ class LaraAppServiceProvider extends ServiceProvider
 	 */
 	public function boot()
 	{
+
+		// see: https://github.com/mcamara/laravel-localization?tab=readme-ov-file#caching-routes
+		RouteServiceProvider::loadCachedRoutesUsing(fn () => $this->loadCachedRoutes());
 
 		// Load Translations
 		$this->loadTranslationsFrom(__DIR__ . '/../Resources/lang', 'lara-app');
