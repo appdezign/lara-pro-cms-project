edae16681f084893b6d535802340865a3b62bc2c
Sybrand Hoeksma <s.hoeksma@firmaq.nl>
2026-02-18 10:25:30 +0100

    Update Mason Bricks

--- config/mason.php
+++ config/mason.php
@@ -4,7 +4,16 @@ declare(strict_types=1);
 
 return [
     'generator' => [
-        'namespace' => 'App\\Mason',
-        'views_path' => 'mason',
+        'namespace' => 'Lara\\App\Filament\\Components\\Mason',
+        'views_path' => 'lara-app',
+    ],
+    'preview' => [
+        'layout' => 'lara-app::components.mason.iframe-preview', // Set to your layout view path, e.g., 'layouts.preview'
+    ],
+    'entry' => [
+        'layout' => 'lara-app::components.mason.iframe-entry', // Set to your layout view path, e.g., 'layouts.entry'
+    ],
+    'routes' => [
+        'middleware' => ['web', 'auth'],
     ],
 ];
