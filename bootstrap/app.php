9a2127cf72373e2a030ec4caf17b0479a528aaa0
Sybrand Hoeksma <s.hoeksma@firmaq.nl>
2026-02-22 18:25:42 +0100

    Dynamic public folder for bootstrap

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -5,6 +5,13 @@ use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
+	->registered(function ($app) {
+		// Set public path on Plesk Server
+		// $app->usePublicPath(path: realpath(base_path('../httpdocs')));
+
+		// Set public path on Servebolt Server
+		// $app->usePublicPath(path: realpath(base_path('../public')));
+	})
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         commands: __DIR__.'/../routes/console.php',
