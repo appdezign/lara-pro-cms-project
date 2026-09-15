001aec507147bd43e6ef9adf4aecf765b3fa9fa3
Sybrand Hoeksma <s.hoeksma@firmaq.nl>
2026-09-15 09:25:08 +0200

    Better maintenance page

--- resources/views/vendor/filament-panels/components/header/simple.blade.php
+++ resources/views/vendor/filament-panels/components/header/simple.blade.php
@@ -1,3 +1,8 @@
+<?php
+use Composer\InstalledVersions;
+$laraVersion = InstalledVersions::getPrettyVersion('appdezign/lara-pro-cms');
+?>
+
 @props([
     'heading' => null,
     'logo' => true,
@@ -6,4 +11,5 @@
 
 <header class="fi-simple-header">
 	<img alt="Lara 10" src="/assets/filament/img/lara10-logo.png" style="height: 3rem;" class="fi-logo">
+	<div class="absolute w-100 bottom-0 py-2 text-sm  text-gray-500 text-center">v{{ $laraVersion }}</div>
 </header>
